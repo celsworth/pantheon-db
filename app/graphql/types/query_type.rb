61a739337be933e66423614cf1089dfa5429eaef
Chris Elsworth <chris@cae.me.uk>
2024-02-15 15:26:15 +0000

    ItemSearchResolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,23 +4,7 @@ module Types
   class QueryType < Types::BaseObject
     field :item, resolver: Resolvers::ItemResolver
     field :items, resolver: Resolvers::ItemsResolver
-
-    field :item_search, [Types::ItemType] do
-      argument :name, String, required: false
-      argument :category, String, required: false
-      argument :slot, String, required: false
-      argument :class, String, required: false
-
-      argument :dropped_by, ID, required: false
-      argument :reward_from_quest, ID, required: false
-
-      argument :stats, [Inputs::StatInputFilterType], required: false
-      argument :required_level, [Inputs::FloatOperatorInputFilterType], required: false
-      argument :weight, [Inputs::FloatOperatorInputFilterType], required: false
-    end
-    def item_search(**params)
-      ItemSearch.new(**params).search.all
-    end
+    field :item_search, resolver: Resolvers::ItemSearchResolver
 
     field :monster, resolver: Resolvers::MonsterResolver
     field :monsters, resolver: Resolvers::MonstersResolver
