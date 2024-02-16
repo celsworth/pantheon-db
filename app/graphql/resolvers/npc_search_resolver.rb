308b2fb6f0bd911a063ca27b2f739da6a9e7ed70
Chris Elsworth <chris@cae.me.uk>
2024-02-16 16:49:33 +0000

    Finish NPC search

--- app/graphql/resolvers/npc_search_resolver.rb
+++ app/graphql/resolvers/npc_search_resolver.rb
@@ -5,7 +5,13 @@ module Resolvers
     type [Types::NpcType], null: false
 
     argument :name, String, required: false
+    argument :subtitle, String, required: false
     argument :vendor, Boolean, required: false
+    argument :zone_id, ID, required: false
+
+    argument :gives_quest, ID, required: false
+    argument :receives_quest, ID, required: false
+    argument :sells_item, ID, required: false
 
     def resolve(**params)
       NpcSearch.new(**params).search.all
---
+++ app/services/npc_search.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+class NpcSearch
+  include ActiveRecord::Sanitization::ClassMethods
+
+  InvalidOperator = Class.new(StandardError)
+
+  FILTERS = %i[
+    filter_name filter_subtitle filter_vendor filter_zone
+    filter_gives_quest filter_receives_quest filter_sells_item
+  ].freeze
+
+  def initialize(**params)
+    @params = params
+  end
+
+  def search
+    @dataset = Npc
+
+    FILTERS.each { send(_1) }
+
+    @dataset
+  end
+
+  private
+
+  def filter_name
+    where('name ILIKE ?', "%#{sanitize_sql_like(@params[:name])}%") if @params[:name]
+  end
+
+  def filter_subtitle
+    where('subtitle ILIKE ?', "%#{sanitize_sql_like(@params[:subtitle])}%") if @params[:subtitle]
+  end
+
+  def filter_vendor
+    return if @params[:vendor].nil?
+
+    where(vendor: @params[:vendor])
+  end
+
+  def filter_zone
+    return if @params[:zone_id].nil?
+
+    where(zone_id: @params[:zone_id])
+  end
+
+  def filter_gives_quest
+    return unless @params[:gives_quest]
+
+    ids = Npc.joins(:quests_given).where('quests_given.id': @params[:gives_quest])
+    where(id: ids)
+  end
+
+  def filter_receives_quest
+    return unless @params[:receives_quest]
+
+    ids = Npc.joins(:quests_received).where('quests_given.id': @params[:receives_quest])
+    where(id: ids)
+  end
+
+  def filter_sells_item
+    return unless @params[:sells_item]
+
+    ids = Npc.joins(:sells_items).where('sells_items.id': @params[:sells_item])
+    where(id: ids)
+  end
+
+  def where(...)
+    @dataset = @dataset.where(...)
+  end
+end
