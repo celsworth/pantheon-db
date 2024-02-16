e0fcff914e80965b8a026c311cfbbe77ece7ee3d
Chris Elsworth <chris@cae.me.uk>
2024-02-16 21:41:11 +0000

    simplify resolvers that have a search

--- app/graphql/resolvers/npcs_resolver.rb
+++ app/graphql/resolvers/npcs_resolver.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 module Resolvers
-  class NpcsResolver < BaseResolver
+  class NpcSearchResolver < BaseResolver
     type [Types::NpcType], null: false
 
-    def resolve
-      Npc.all
+    argument :name, String, required: false
+    argument :subtitle, String, required: false
+    argument :vendor, Boolean, required: false
+    argument :zone_id, ID, required: false
+
+    argument :gives_quest, ID, required: false
+    argument :receives_quest, ID, required: false
+    argument :sells_item, ID, required: false
+
+    def resolve(**params)
+      NpcSearch.new(**params).search.all
     end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,12 @@ module Types
   class QueryType < Types::BaseObject
     field :item, resolver: Resolvers::ItemResolver
     field :items, resolver: Resolvers::ItemsResolver
-    field :item_search, resolver: Resolvers::ItemSearchResolver
 
     field :monster, resolver: Resolvers::MonsterResolver
     field :monsters, resolver: Resolvers::MonstersResolver
 
     field :npc, resolver: Resolvers::NpcResolver
     field :npcs, resolver: Resolvers::NpcsResolver
-    field :npc_search, resolver: Resolvers::NpcSearchResolver
 
     field :quest, resolver: Resolvers::QuestResolver
     field :quests, resolver: Resolvers::QuestsResolver
