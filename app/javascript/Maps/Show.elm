21405d6569ef49516d55a309630659f2e4e501b0
Chris Elsworth <chris@cae.me.uk>
2024-02-25 21:42:52 +0000

    WIP on map POI plotting

--- app/javascript/Maps/Show.elm
+++ app/javascript/Maps/Show.elm
@@ -10,10 +10,25 @@ import Svg.Attributes
 import Types exposing (Loc)
 
 
+
+-- dire lord: map x = 480, map y = 297
+-- well: map x = 968, map y = 752
+-- map X size = 2800
+-- map Y size = 2080
+-- thronefast top left point = 2500, 4500 (or 4520?)
+-- so bottom left is 2500, 2440
+
+
 locs : List ( String, Loc )
 locs =
-    [ ( "Rabbit", { x = 3762, z = 555, y = 3070 } )
+    [ ( "Huntress", { x = 3366, z = 555, y = 3634 } )
+    , ( "Dire Lord Scrolls", { x = 2962, z = 476, y = 4222 } )
     , ( "Scavenger", { x = 3455, z = 476, y = 3771 } )
+    , ( "Artificer", { x = 3384, z = 476, y = 3739 } )
+    , ( "Shaman Scrolls", { x = 3026, z = 476, y = 3776 } )
+    , ( "Tailor", { x = 3457, z = 476, y = 3677 } )
+    , ( "Weapon Master", { x = 3419, z = 476, y = 3726 } )
+    , ( "Borra", { x = 2996, z = 476, y = 3823 } )
     ]
 
 
@@ -39,12 +54,12 @@ type alias Offset =
 
 mapXSize : number
 mapXSize =
-    1152
+    2800
 
 
 mapYSize : number
 mapYSize =
-    864
+    2080
 
 
 type DragData
@@ -227,12 +242,12 @@ view model =
             []
         , div
             (class "section map-container" :: mouseEvents)
-            [ svgTest model ]
+            [ svgView model ]
         ]
 
 
-svgTest : Model -> Html Msg
-svgTest model =
+svgView : Model -> Html Msg
+svgView model =
     let
         -- entire viewport is eg 1152x864, so use zoom to reduce those
         -- ie zoom of 2 would be showing eg 576x432
@@ -253,26 +268,54 @@ svgTest model =
     in
     svg
         [ Svg.Attributes.class "svg"
-        , Svg.Attributes.width <| String.fromInt mapXSize
-        , Svg.Attributes.height <| String.fromInt mapYSize
+        , Svg.Attributes.width "1152"
+        , Svg.Attributes.height "864"
         , Svg.Attributes.viewBox viewBox
         ]
-        [ Svg.image
+        (Svg.image
             [ Svg.Attributes.xlinkHref "https://cdn.discordapp.com/attachments/1175493372430000218/1210710301767507998/Thronefast_2024-02-14.png?ex=65eb8cd5&is=65d917d5&hm=b5991cb0c52746a90fe644314bd2fdc7d1c4eb85edd96e9339b1c5c31385e9d5&"
             , Svg.Attributes.width <| String.fromInt mapXSize
             , Svg.Attributes.height <| String.fromInt mapYSize
             ]
             []
-        , Svg.circle
-            [ Svg.Attributes.cx "365"
-            , Svg.Attributes.cy "105"
-            , Svg.Attributes.r "3"
-            , Svg.Attributes.stroke "#000"
-            , Svg.Attributes.fill "#CCCC00"
-            ]
-            []
-        , Svg.text_ [ Svg.Attributes.x "375", Svg.Attributes.y "115", Svg.Attributes.style "stroke:white; stroke-width:0.6em; fill:black; paint-order:stroke; stroke-linejoin:round" ] [ Html.text "Caesium's Island" ]
+            :: pois
+        )
+
+
+pois : List (Svg Msg)
+pois =
+    locs
+        |> List.map (\( name, loc ) -> [ poiCircle loc, poiText name loc ])
+        |> List.concat
+
+
+poiCircle : Loc -> Svg Msg
+poiCircle loc =
+    Svg.circle
+        [ Svg.Attributes.cx <| String.fromFloat loc.x
+        , Svg.Attributes.cy <| String.fromFloat loc.y
+        , Svg.Attributes.r "3"
+        , Svg.Attributes.stroke "#000"
+        , Svg.Attributes.fill "#CCCC00"
         ]
+        []
+
+
+poiText : String -> Loc -> Svg Msg
+poiText name loc =
+    let
+        offsetLocX =
+            loc.x - 2500
+
+        offsetLocY =
+            4520 - loc.y
+    in
+    Svg.text_
+        [ Svg.Attributes.x <| String.fromFloat offsetLocX
+        , Svg.Attributes.y <| String.fromFloat offsetLocY
+        , Svg.Attributes.style "stroke:white; stroke-width:0.6em; fill:black; paint-order:stroke; stroke-linejoin:round"
+        ]
+        [ Html.text name ]
 
 
 subscriptions : Model -> Sub Msg
