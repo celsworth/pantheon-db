5996c3947091fc98adf16f7d2816c5299d56bdc6
Chris Elsworth <chris@cae.me.uk>
2024-02-28 14:55:46 +0000

    more resource filters

--- app/graphql/resolvers/resources_resolver.rb
+++ app/graphql/resolvers/resources_resolver.rb
@@ -9,10 +9,15 @@ module Resolvers
     argument :id, ID, required: false, description: 'Filter to the given Resource ID'
     argument :name, String, required: false, description: 'Filter to matching Resource names'
 
-    def resolve(id: nil, name: nil)
+    argument :resource, Types::ResourceResourceType, required: false
+    argument :size, Types::ResourceSizeType, required: false
+
+    def resolve(id: nil, name: nil, resource: nil, size: nil)
       dataset = Resource
       dataset = dataset.where(id:) if id
       dataset = dataset.where('name ILIKE ?', "%#{sanitize_sql_like(name)}%") if name
+      dataset = dataset.where(resource:) if resource
+      dataset = dataset.where(size:) if size
       dataset.all
     end
   end
