0cf965a0b851e872f45049c1bc39bcdd565fbe68
Chris Elsworth <chris@cae.me.uk>
2024-03-01 19:07:37 +0000

    support multi-resource type and size

--- app/graphql/resolvers/resources_resolver.rb
+++ app/graphql/resolvers/resources_resolver.rb
@@ -7,10 +7,13 @@ module Resolvers
     type [Types::ResourceType], null: false
 
     argument :id, ID, required: false, description: 'Filter to the given Resource ID'
-    argument :name, String, required: false, description: 'Filter to matching Resource names'
-
-    argument :resource, Types::ResourceResourceType, required: false
-    argument :size, Types::ResourceSizeType, required: false
+    argument :name, String, required: false, description: 'Filter to matching Resource name'
+    argument :resource, [Types::ResourceResourceType], required: false, description: <<~DESC
+      Filter to any matching Resource types
+    DESC
+    argument :size, [Types::ResourceSizeType], required: false, description: <<~DESC
+      Filter to any matching Resource sizes
+    DESC
 
     def resolve(id: nil, name: nil, resource: nil, size: nil)
       dataset = Resource
