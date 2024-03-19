52a6cef86ef098cb8e5b4003807c67c9482ecbbd
Chris Elsworth <chris@cae.me.uk>
2024-03-19 19:07:31 +0000

    cope with caching users?

--- config/initializers/graphql_fragment_cache.rb
+++ config/initializers/graphql_fragment_cache.rb
@@ -5,6 +5,7 @@ expires_in = Rails.env.production? ? 10.minutes : 1.second
 GraphQL::FragmentCache.configure do |config|
   config.default_options = {
     cache_key: -> { Rails.cache.fetch('graphql_cache_key') },
+    cache_fragment: { context_key: :current_user },
     expires_in:
   }
 end
