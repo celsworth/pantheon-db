0c358dd0a55a080b39432d2f7e213af78ccc0ea7
Chris Elsworth <chris@cae.me.uk>
2024-03-24 18:10:17 +0000

    monster edit form hookup

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -9,6 +9,16 @@ class MonstersController < ApplicationController
 
   def edit; end
 
+  def update
+    p params
+    if monster.update(monster_params)
+      # redirect_to monster, notice: 'Saved!'
+      redirect_to edit_monster_path(monster)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def test
     locations = Location.where(category: params[:category])
     render partial: 'monsters/location',
@@ -17,5 +27,12 @@ class MonstersController < ApplicationController
 
   def monster
     @monster ||= Monster.find_by(id: params[:id])
+  end
+
+  def monster_params
+    params.require(:monster).permit(
+      :name, :level, :loc_x, :loc_y, :loc_z, :public_notes, :private_notes,
+      :named, :elite, :location_id, :roamer
+    )
   end
 end
