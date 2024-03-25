9623af5c828f6c649ebeba9de3eb8c81ac036f23
Chris Elsworth <chris@cae.me.uk>
2024-03-25 08:07:30 +0000

    WIP on viewing monsters

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -16,7 +16,7 @@ class MonstersController < ApplicationController
   def update
     return head 403 unless can? :edit, monster
 
-    if monster.update(monster_params)
+    if update_monster
       # redirect_to monster, notice: 'Saved!'
       redirect_to edit_monster_path(monster), notice: 'Changes Saved!'
     else
@@ -30,14 +30,26 @@ class MonstersController < ApplicationController
            locals: { selected_id: params[:selected_id], locations: }
   end
 
+  private
+
   def monster
     @monster ||= Monster.find_by(id: params[:id])
+  end
+
+  def update_monster
+    Monster.transaction do
+      screenshot = params[:screenshot]
+      data = screenshot.read
+      image = Image.create(data:, size: data.length, mime: screenshot.content_type)
+      monster.update(monster_params)
+      monster.images << image
+    end
   end
 
   def monster_params
     params.require(:monster).permit(
       :name, :level, :loc_x, :loc_y, :loc_z, :public_notes, :private_notes,
-      :named, :elite, :location_id, :roamer
+      :named, :elite, :location_id, :roamer, :screenshot
     ).tap do |p|
       p[:named] = (p[:named] == 'on')
       p[:elite] = (p[:elite] == 'on')
